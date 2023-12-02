79cac7109c03fff41142d0ab0b6afb4461a6627a
Arpit Jana <arpitjana2103@gmail.com>
2023-12-02 09:20:39 +0530

    DB Connection Established

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-undef */
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -8,8 +7,10 @@ import dotenv from 'dotenv';
 const app = express();
 // Setting-up ENV path
 dotenv.config({path: './config.env'});
-const DB = process.env.DB;
+
+// const DB = process.env.DB;
 const DBLocal = process.env.DBLocal;
+const PORT = process.env.PORT;
 
 // Controls the maximum request body size.
 app.use(bodyParser.json({limit: '30mb', extended: true}));
@@ -17,5 +18,16 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
 
 app.use(cors());
 
-console.log(DB);
-console.log(DBLocal);
+console.log('Connecting with DB..');
+
+mongoose
+    .connect(DBLocal)
+    .then(function () {
+        app.listen(PORT, function () {
+            console.log('DB Connection Successfull.');
+            console.log('Server is running on PORT : ' + PORT);
+        });
+    })
+    .catch(function () {
+        console.log('ERROR : DB Connection Error');
+    });
