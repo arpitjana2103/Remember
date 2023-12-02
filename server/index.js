e8253e80be63a7a926920ea2ca6a0b05b2229bbd
Arpit Jana <arpitjana2103@gmail.com>
2023-12-02 08:30:18 +0530

    minor changes

--- server/index.js
+++ server/index.js
@@ -4,15 +4,3 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
-
-const app = express();
-
-// Setting-up ENV path
-dotenv.config({path: './config.env'});
-const DB = process.env.DB;
-const DBLocal = process.env.DBLocal;
-
-// Controls the maximum request body size.
-app.use(bodyParser.json({limit: '30mb', extended: true}));
-app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
-app.use(cors());
