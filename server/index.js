ae2345840f61846f7097f59e66f1eb73683b718d
Arpit Jana <arpitjana2103@gmail.com>
2023-12-02 08:25:30 +0530

    Server - env, bodyparser, cors setup completed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
+/* eslint-disable no-undef */
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
+
+const app = express();
+
+// Setting-up ENV path
+dotenv.config({path: './config.env'});
+const DB = process.env.DB;
+const DBLocal = process.env.DBLocal;
+
+// Controls the maximum request body size.
+app.use(bodyParser.json({limit: '30mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '30mb', extended: true}));
+app.use(cors());
